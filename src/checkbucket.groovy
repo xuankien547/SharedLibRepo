286138b587b25886047f062138ba4c431ea7ccd0
Kien <xuankien547@gmail.com>
2025-07-30 10:54:13 +0700

    update

--- src/checkbucket.groovy
+++ src/checkbucket.groovy
@@ -1,10 +1,19 @@
 def call(String bucketName) {
-    def s3 = new com.amazonaws.services.s3.AmazonS3Client()
-    def exists = s3.doesBucketExistV2(bucketName)
-    
-    if (exists) {
-        println "Bucket '${bucketName}' exists."
-    } else {
-        println "Bucket '${bucketName}' does not exist."
+    pipeline {
+        agent any
+        stages {
+            stage('Check Bucket') {
+                steps {
+                    script {
+                        def bucketExists = sh(script: "aws s3api head-bucket --bucket ${bucketName} 2>/dev/null", returnStatus: true) == 0
+                        if (bucketExists) {
+                            echo "Bucket '${bucketName}' exists."
+                        } else {
+                            error "Bucket '${bucketName}' does not exist."
+                        }
+                    }
+                }
+            }
+        }
     }
 }
